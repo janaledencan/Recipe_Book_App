707bd710c6da511d5d75740cba5236fb98958173
janaledencan <janaledencan@gmail.com>
2024-03-30 20:14:03 +0100

    Display the result of the API response on the page - it does not work properly, the API call is commented out

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,37 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Popular() {
 
+    const [popular, setPopular] = useState([]);
 
     useEffect(() => {
         getPopular();
     }, []);
 
     const getPopular = async () => {
-        //const api = await fetch(`https://low-carb-recipes.p.rapidapi.com/random?apiKey=${process.env.REACT_APP_API_KEY}`);
-        //const data = await api.json();
-        //console.log(data);
 
-        const url = 'https://low-carb-recipes.p.rapidapi.com/random';
-        const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': `${process.env.REACT_APP_API_KEY}`,
-                'X-RapidAPI-Host': 'low-carb-recipes.p.rapidapi.com'
-            }
-        };
+        // const url = 'https://low-carb-recipes.p.rapidapi.com/random';
+        // const options = {
+        //     method: 'GET',
+        //     headers: {
+        //         'X-RapidAPI-Key': `${process.env.REACT_APP_API_KEY}`,
+        //         'X-RapidAPI-Host': 'low-carb-recipes.p.rapidapi.com'
+        //     }
+        // };
 
-        try {
-            const response = await fetch(url, options);
-            const data = await response.json(); //response.text();
-            console.log(data);
-        } catch (error) {
-            console.error(error);
-        }
+        // try {
+        //     const response = await fetch(url, options);
+        //     const data = await response.json(); //response.text();
+        //     //console.log(data);
+        //     setPopular(data);
+        // } catch (error) {
+        //     console.error(error);
+        // }
     }
 
     return (
-        <div>Popular</div>
+        <div>
+            {/* <p>{popular.name}</p> */}
+        </div>
+
     )
 }
 
