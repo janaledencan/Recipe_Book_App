62bde570a3a74fcdf4c2efd8b6509ab535da094a
janaledencan <janaledencan@gmail.com>
2024-06-11 19:32:24 +0200

    API works, add imports for framer-motion  react-icons react-router-dom styled-components

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -10,28 +10,28 @@ function Popular() {
 
     const getPopular = async () => {
 
-        // const url = 'https://low-carb-recipes.p.rapidapi.com/random';
-        // const options = {
-        //     method: 'GET',
-        //     headers: {
-        //         'X-RapidAPI-Key': `${process.env.REACT_APP_API_KEY}`,
-        //         'X-RapidAPI-Host': 'low-carb-recipes.p.rapidapi.com'
-        //     }
-        // };
+        const url = 'https://low-carb-recipes.p.rapidapi.com/random';
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': `${process.env.REACT_APP_API_KEY}`,
+                'X-RapidAPI-Host': 'low-carb-recipes.p.rapidapi.com'
+            }
+        };
 
-        // try {
-        //     const response = await fetch(url, options);
-        //     const data = await response.json(); //response.text();
-        //     //console.log(data);
-        //     setPopular(data);
-        // } catch (error) {
-        //     console.error(error);
-        // }
+        try {
+            const response = await fetch(url, options);
+            const data = await response.json(); //response.text();
+            //console.log(data);
+            setPopular(data);
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     return (
         <div>
-            {/* <p>{popular.name}</p> */}
+            <p>{popular.name}</p>
         </div>
 
     )
