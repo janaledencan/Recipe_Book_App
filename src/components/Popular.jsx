4f74caad4984941ac82d4569b90f3c075ec4dd64
janaledencan <janaledencan@gmail.com>
2024-06-12 13:06:07 +0200

    Add saving the received API response in local storage so you don't waste calls

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -14,11 +14,22 @@ function Popular() {
 
 
   const getPopular = async () => {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-    );
-    const data = await api.json();
-    setPopular(data.recipes);
+
+    const check = localStorage.getItem('popular');
+
+    if (check) {
+      setPopular(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+      );
+      const data = await api.json();
+
+      localStorage.setItem("popular", JSON.stringify(data.recipes));
+      setPopular(data.recipes);
+      console.log(data.recipes);
+    }
+
   };
 
   return (
@@ -34,8 +45,8 @@ function Popular() {
         }}>
           {popular.map((recipe) => {
             return (
-              <SplideSlide>
-                <Card key={recipe.id}>
+              <SplideSlide key={recipe.id}>
+                <Card>
                   <p>{recipe.title}</p>
                   <img src={recipe.image} alt={recipe.title} />
                   <Gradient />
