ec009fc97a5ca67133382f4f6f0d00efe9acb26c
janaledencan <janaledencan@gmail.com>
2024-06-23 14:48:53 +0200

    Style cusine button links

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,25 +8,25 @@ import { NavLink } from "react-router-dom";
 function Navigation() {
     return (
         <List>
-            <NavLink to={'/cuisine/Italian'}>
+            <SLink to={'/cuisine/Italian'}>
                 <GiCook />
                 <h4>Main</h4>
-            </NavLink>
+            </SLink>
 
-            <NavLink to={'/cuisine/American'}>
+            <SLink to={'/cuisine/American'}>
                 <FaRegHeart />
                 <h4>My Favorite</h4>
-            </NavLink >
+            </SLink >
 
-            <NavLink to={'/cuisine/Italian'}>
+            <SLink to={'/cuisine/Italian'}>
                 <GiCook />
                 <h4>Main</h4>
-            </NavLink>
+            </SLink>
 
-            <NavLink to={'/cuisine/American'}>
+            <SLink to={'/cuisine/American'}>
                 <FaRegHeart />
                 <h4>My Favorite</h4>
-            </NavLink>
+            </SLink>
         </List>
     )
 }
@@ -36,6 +36,30 @@ const List = styled.div`
     display:flex;
     justify-content: center;
     margin: 2rem 0rem;
+`;
+
+const SLink = styled(NavLink)`
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    margin-right: 2rem;
+    text-decoration: none;
+    width: 6rem;
+    cursor: pointer;
+    transform: scale(0.8);
+
+
+    h4{
+        font-size:0.8rem;
+    }
+
+    svg{
+        color: #8b0000;
+        font-size: 1.5rem;
+    }
+
 `;
 
 export default Navigation;
