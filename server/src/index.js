9755d9755e94c24e665f41b22b63248f31127475
janaledencan <janaledencan@gmail.com>
2024-06-25 20:33:43 +0200

    Add MongoDB connection string, add password in environment variable

--- server/src/index.js
+++ server/src/index.js
@@ -3,10 +3,11 @@ import cors from "cors";
 import mongoose from "mongoose";
 
 const app = express()
+const password = process.env.MONGODB_PASSWORD;
 
 app.use(express.json) //middleware
 app.use(cors());
 
-
+mongoose.connect(`mongodb+srv://janaLed:${password}@recipes.wd5trug.mongodb.net/?retryWrites=true&w=majority&appName=recipes`);
 
 app.listen(3001, () => console.log("SERVER STARTED!")); 
