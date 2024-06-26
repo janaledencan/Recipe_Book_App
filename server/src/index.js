741d51cce09ab380f97f2d83c2623bd19594c6be
janaledencan <janaledencan@gmail.com>
2024-06-26 21:10:30 +0200

    Create API and corresponding registration and login routes using express and MongoDB Atlas

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,18 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import { userRouter } from './routes/users.js';
+
+dotenv.config();
 
 const app = express()
 const password = process.env.MONGODB_PASSWORD;
 
 app.use(express.json) //middleware
 app.use(cors());
+
+app.use("/auth", userRouter);
 
 mongoose.connect(`mongodb+srv://janaLed:${password}@recipes.wd5trug.mongodb.net/?retryWrites=true&w=majority&appName=recipes`);
 
