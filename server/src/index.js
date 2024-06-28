9d95521f930f31cf08bc5c8f5061dab2cf5c0eca
janaledencan <janaledencan@gmail.com>
2024-06-28 22:07:14 +0200

    Create a backend part for the user's recipes - creating and saving a new recipe, retrieving recipes and their IDs

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import { userRouter } from './routes/users.js';
+import { recipesRouter } from './routes/recipes.js';
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(express.json()); //middleware
 app.use(cors());
 
 app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
 
 mongoose.connect(
     `mongodb+srv://janaLed:${password}@recipes.wd5trug.mongodb.net/?retryWrites=true&w=majority&appName=recipes`
